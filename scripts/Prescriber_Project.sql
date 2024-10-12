dbfc58101a2b110f96a0eae24ba06c670f215630
JesseCrawford89 <jesse.s.crawford89@gmail.com>
2024-10-12 13:52:53 -0500

    Post-in class review

--- scripts/Prescriber_Project.sql
+++ scripts/Prescriber_Project.sql
@@ -15,8 +15,7 @@ ORDER BY Total_Claim_Count DESC
 --b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name, specialty_description, and the total number of claims.
 
 Select 	p1.npi
-	, 	p1.nppes_provider_last_org_name AS Provider
-	,	p1.nppes_provider_first_name AS First_Name
+	,	CONCAT(p1.nppes_provider_first_name, ' ', p1.nppes_provider_last_org_name) AS Name
 	,	p1.specialty_description
 	, 	SUM(p2.total_claim_count) AS Total_Claim_Count 
 FROM prescriber AS p1
@@ -78,21 +77,20 @@ GROUP BY specialty, prescription
 
 --a. Which drug (generic_name) had the highest total drug cost?
 
-SELECT 	p.drug_name AS Name
-	,	d.generic_name
-	,	SUM(p.total_drug_cost) AS Cost
+SELECT 	d.generic_name
+	,	SUM(p.total_drug_cost)::MONEY AS Cost
 FROM prescription AS p
 LEFT JOIN drug AS d
 	USING (drug_name)
-GROUP BY drug_name, generic_name
+GROUP BY generic_name
 ORDER BY Cost DESC
 
---Answer: Pregabalin at 78,645,939.89
+--Answer: Insulin Glargine cost $104,264,066.35
 
 --b. Which drug (generic_name) has the hightest total cost per day? Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
 
 SELECT  d.generic_name
-    ,   ROUND(SUM(p.total_drug_cost)/SUM(p.total_day_supply), 2) AS Daily_Cost
+    ,   ROUND(SUM(p.total_drug_cost)/SUM(p.total_day_supply), 2)::MONEY AS Daily_Cost
 FROM drug AS d
 LEFT JOIN prescription AS p
         USING (drug_name)
@@ -100,7 +98,7 @@ WHERE p.total_drug_cost IS NOT NULL
 GROUP BY d.generic_name
 ORDER BY Daily_Cost DESC
 
---Answer: C1 Esterase Inhibitor has the highest cost per day at 3495.22
+--Answer: C1 Esterase Inhibitor has the highest cost per day at $3,495.22
 
 --Question 4
 
@@ -139,18 +137,19 @@ ORDER BY Total_Cost DESC
 
 --a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
 
-SELECT COUNT(c.cbsa) AS Num_of_CBSA, f.state
+SELECT COUNT(DISTINCT c.cbsa) AS Num_of_CBSA, f.state
 FROM cbsa AS c
 LEFT JOIN fips_county AS f
 	USING(fipscounty)
 WHERE f.state = 'TN'
 GROUP BY f.state
 
---Answer: There are 42 CBSAs in TN.
+--Answer: There are 10 CBSAs in TN.
 
 --b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
 
-SELECT c.cbsaname, SUM(p.population) AS population
+SELECT 	c.cbsaname
+	,	SUM(p.population) AS population
 FROM cbsa AS c
 LEFT JOIN population AS p
 	USING (fipscounty)
@@ -164,14 +163,16 @@ ORDER BY population DESC
 
 --c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
 
-SELECT SUM(p.population) AS population, f.county, c.cbsa AS CBSA
+SELECT 	SUM(p.population) AS population
+	, 	f.county
 FROM population AS p
 FULL JOIN fips_county AS f
 	USING(fipscounty)
 FULL JOIN cbsa AS c
 	USING(fipscounty)
-WHERE p.population IS NOT NULL AND c.cbsa IS NULL
-GROUP BY f.county, c.cbsa
+WHERE 	p.population IS NOT NULL 
+	AND c.cbsa IS NULL
+GROUP BY f.county
 ORDER BY population DESC
 
 --Answer: Sevier County is the largest county by population that is not included in a CBSA.
@@ -182,11 +183,18 @@ ORDER BY population DESC
 
 SELECT p.drug_name, SUM(p.total_claim_count)
 FROM prescription AS p
-WHERE p.total_claim_count >3000
+WHERE p.total_claim_count >=3000
 GROUP BY p.drug_name
 ORDER BY SUM(p.total_claim_count) DESC
 
---Answer: There are 7 prescriptions with a total claim count over 3000.
+SELECT p.drug_name, p.total_claim_count, p.npi
+FROM prescription AS p
+WHERE p.total_claim_count >=3000
+--GROUP BY p.drug_name, p.npi
+ORDER BY p.total_claim_count DESC
+
+
+--Answer: There are 7 prescriptions with a total claim count over 3000. Bottom query returns 9 rows due to different npis.
 /* 	1. LEVOTHYROXINE SODIUM 		@ 9262
 	2. OXYCODONE HCL				@ 4538
 	3. LISINOPRIL					@ 3655
@@ -208,14 +216,19 @@ ORDER BY SUM(p.total_claim_count) DESC
 
 --c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
 
-SELECT CONCAT(p2.nppes_provider_first_name,' ',p2.nppes_provider_last_org_name) AS Provider_Name, p1.drug_name AS Drug, SUM(p1.total_claim_count) AS Total_Claim_Count, d.opioid_drug_flag AS Opioid
+SELECT 	CONCAT(p2.nppes_provider_first_name,' ',p2.nppes_provider_last_org_name) AS Provider_Name
+	, 	p1.drug_name AS Drug
+	, 	SUM(p1.total_claim_count) AS Total_Claim_Count
+	, 	d.opioid_drug_flag AS Opioid
 FROM prescription AS p1
 RIGHT JOIN drug as d
 	USING(drug_name)
 RIGHT JOIN prescriber AS p2
 	USING(npi)
 WHERE p1.total_claim_count >3000
-GROUP BY p1.drug_name, d.opioid_drug_flag, p2.nppes_provider_first_name, p2.nppes_provider_last_org_name
+GROUP BY 	p1.drug_name, d.opioid_drug_flag
+	, 		p2.nppes_provider_first_name
+	, 		p2.nppes_provider_last_org_name
 ORDER BY SUM(p1.total_claim_count) DESC
 
 --Question 7: The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. Hint: The results from all 3 parts will have 637 rows.
@@ -223,13 +236,13 @@ ORDER BY SUM(p1.total_claim_count) DESC
 --a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). Warning: Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
 
 
-SELECT p.npi, d.drug_name
+SELECT 	p.npi
+	, 	d.drug_name
 FROM prescriber as p
 CROSS JOIN drug as d
-WHERE p.specialty_description ='Pain Management' 
-AND p.nppes_provider_city = 'NASHVILLE' 
-AND d.opioid_drug_flag = 'Y'
-GROUP BY p.npi, d.drug_name
+	WHERE 	p.specialty_description ='Pain Management' 
+	AND 	p.nppes_provider_city = 'NASHVILLE' 
+	AND 	d.opioid_drug_flag = 'Y'
 
 --b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
 
@@ -242,7 +255,8 @@ FROM prescriber as p1
 WHERE p1.specialty_description ='Pain Management'
 		AND p1.nppes_provider_city = 'NASHVILLE'
 		AND d.opioid_drug_flag = 'Y'
-GROUP BY p1.npi, d.drug_name
+GROUP BY 	p1.npi
+	, 		d.drug_name
 ORDER BY npi DESC
 
 --c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
